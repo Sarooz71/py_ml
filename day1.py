ad4ceb31b4cd50a7f419a58720805ebc9a903a29
Saroj lama <tamangsarooz100@gmail.com>
2026-06-01 06:26:36 +0545

    day 1 python workshop

---
+++ day1.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+# name = "RAM"
+# print(name + " is saying hello" )
+
+# print(f"Hello, {name}!")
+
+name = "Raja Babu"
+faculty = "Science"
+dob = "2059/00/12"
+# formate = f
+print(f"I came from {faculty} with the day born {dob} remember me {name}"  )
+
+#Data type
+print(f"type of name : {type(name)}")
+print(f"type of faculty : {type(faculty)}")
+print(f"type of dob : {type(dob)}")
+# print(f"type of age : {type(age)}")
+# print(f"type of is_student : {type(is_student)}") #bool
+
+# Swap variable easily
+
+# x,y = 10, 20
+# print("before swap: x=" , x "y=", y)
+# x, y = y,x
+# print("After swap: x=", x, "y=", y)
+
+# Unpack lists
+student_info = ["Charlie" , 21, 88.0]
+name , age, score = student_info
+print("unpacked:", name, age, score)
